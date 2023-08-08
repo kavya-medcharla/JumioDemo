181b1588621a240ef646f0dc605abe7999d8a173
kavya.medcharla <kavya_28@yahoo.com>
2023-08-08 12:44:33 -0700

    Initial setup

---
+++ JumioApp/JumioApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using JumioApp.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace JumioApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var accountRequest = new AccountRequest
+        {
+            CustomerInternalReference = "Testing",
+            UserReference = "Postman API Tests",
+            TokenLifetime = "5m",
+            WorkflowDefinition = new WorkflowDefinition { Key = 10011 },
+            WebDetails = new WebDetails
+            {
+                SuccessUrl = "https://www.jumio.com",
+                ErrorUrl = "https://jumio.com/error"
+            }
+        };
+
+        var client = _httpClientFactory.CreateClient("jumio");
+        var requestData = new StringContent(JsonConvert.SerializeObject(accountRequest), Encoding.UTF8, "application/json");
+        HttpResponseMessage response = await client.PostAsync("accounts",requestData);
+        var result = await response.Content.ReadAsStringAsync();
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ JumioApp/JumioApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using IdentityModel.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAccessTokenManagement(options =>
+{
+    var tokenReq = new ClientCredentialsTokenRequest
+    {
+        Address = @"https://auth.amer-1.jumio.ai/oauth2/token",
+        //ClientId = "9e12b4ac-3f7b-489e-ba88-b65bc31de693",
+        //ClientSecret = "ErOo0Fr0VKVxtkN1P36JBSQjN1s0KJFN"
+    };
+    tokenReq.Headers.Authorization = new BasicAuthenticationHeaderValue("9e12b4ac-3f7b-489e-ba88-b65bc31de693", "ErOo0Fr0VKVxtkN1P36JBSQjN1s0KJFN");
+    options.Client.Clients.Add("jumio", tokenReq);
+});
+builder.Services.AddClientAccessTokenHttpClient("jumio", configureClient: client =>
+{
+    client.BaseAddress = new Uri("https://account.amer-1.jumio.ai/api/v1/");
+});
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
